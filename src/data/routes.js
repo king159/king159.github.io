a31a4e05df97dee6d97290685a6939364eaeaf95
wjh <zhuce57@126.com>
2023-07-14 22:13:10 +0800

    714

---
+++ src/data/routes.js
@@ -0,0 +1,25 @@
+const routes = [
+  {
+    index: true,
+    label: 'Wang Jinghao',
+    path: '/',
+  },
+  {
+    label: 'About',
+    path: '/about',
+  },
+  {
+    label: 'Resume',
+    path: '/resume',
+  },
+  {
+    label: 'Publication',
+    path: '/publication',
+  },
+  {
+    label: 'Article',
+    path: '/article',
+  },
+];
+
+export default routes;
